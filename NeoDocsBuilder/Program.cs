4e2d3002741319973c97c2009ad7915d12babf6f
陈志同 <chenzhitong12@hotmail.com>
2019-06-17 18:12:12 +0800

    图片文件链接的正确显示

--- NeoDocsBuilder/Program.cs
+++ NeoDocsBuilder/Program.cs
@@ -12,6 +12,7 @@ namespace NeoDocsBuilder
         static void Main(string[] args)
         {
             Files.CopyDirectory(Config.Template, Config.Destination);
+            Files.CopyDirectory(Config.Origin, Config.Destination);
             Files.CopyDirectoryOnly(Config.Origin, Config.Destination);
             Run(Config.Origin, Config.Destination, Config.Template);
             BuildCatalog(Config.Destination);
@@ -41,24 +42,31 @@ namespace NeoDocsBuilder
             {
                 Catalog += "\r\n<li>";
                 var dirName = dir.Split("\\").Reverse().ToList()[0];
-                var newName = Config.FolderJson["rename"]?[dirName]?.ToString();
-                var isHidden = false;
-                foreach (var item in Config.FolderJson["hidden"])
+                if (Config.FolderJson != null)
                 {
-                    if (item.ToString() == dirName)
+                    var newName = Config.FolderJson["rename"][dirName]?.ToString();
+                    var isHidden = false;
+                    foreach (var item in Config.FolderJson["hidden"])
                     {
-                        isHidden = true;
-                        break;
+                        if (item.ToString() == dirName)
+                        {
+                            isHidden = true;
+                            break;
+                        }
                     }
-                }
-                if (isHidden) continue;
-                if (string.IsNullOrEmpty(newName))
-                {
-                    Catalog += $"<span data-icon='+' data-path='{dirName}'>{dirName}</span>";
+                    if (isHidden) continue;
+                    if (string.IsNullOrEmpty(newName))
+                    {
+                        Catalog += $"<span data-icon='+' data-path='{dirName}'>{dirName}</span>";
+                    }
+                    else
+                    {
+                        Catalog += $"<span data-icon='+' data-path='{dirName}'>{newName}</span>";
+                    }
                 }
                 else
                 {
-                    Catalog += $"<span data-icon='+' data-path='{dirName}'>{newName}</span>";
+                    Catalog += $"<span data-icon='+' data-path='{dirName}'>{dirName}</span>";
                 }
                 Run(dir, destination, template);
                 Catalog += "\r\n</li>";
