57d825b808998779922d95e483581af8ba3e3f57
陈志同 <bitcoin2077@outlook.com>
2021-06-23 11:08:45 +0800

    update

--- NeoDocsBuilder/Program.cs
+++ NeoDocsBuilder/Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Toolkit.Parsers.Markdown;
-using Microsoft.Toolkit.Parsers.Markdown.Blocks;
+﻿using Markdig;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -44,12 +43,6 @@ namespace NeoDocsBuilder
 
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            if (!string.IsNullOrEmpty(MdConverter.ErrorLogs.ToString()))
-            {
-                Console.WriteLine(string.Join("\r\n", MdConverter.ErrorLogs.ToArray()));
-                Console.WriteLine($"Content Error Link: {MdConverter.ErrorLogs.Count}");
-            }
-
             if (!string.IsNullOrEmpty(YmlConverter.ErrorLogs.ToString()))
             {
                 Console.WriteLine(string.Join("\r\n", YmlConverter.ErrorLogs.ToArray()));
@@ -102,100 +95,12 @@ namespace NeoDocsBuilder
                 var relativeToOrigin = Path.GetRelativePath(config.Origin, file);
                 //在配置文件中该文档是否根二级标题自动折叠
                 var collapse = config.FolderJson != null && config.FolderJson["collapse"].ToList().Any(p => p.ToString().Equals(relativeToOrigin, StringComparison.OrdinalIgnoreCase));
-                var (title, content, sideNav) = Convert(file, collapse);
                 //生成后的文件路径
                 var newFile = Path.Combine(config.Destination, relativeToOrigin.Replace(".md", ".html")).ToLower();
                 var git = Path.Combine(config.Git, relativeToOrigin);
-                Build(newFile, catalog, content, title, sideNav, git, collapse);
+                var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+                Build(newFile, catalog, Markdown.ToHtml(File.ReadAllText(file), pipeline), "Test", "", git, collapse);
             });
-        }
-
-        /// <summary>
-        /// 对单个 MarkDown 文件进行到 HTML 的转换
-        /// </summary>
-        /// <param name="file">文件名</param>
-        /// <param name="collapse">所有二级标题下面的内容自动折叠，点击二级标题后展开或收缩。True 表示启用</param>
-        /// <returns>输出元组（标题，内容，文章内的目录）</returns>
-        static (string title, string content, string sideNav) Convert(string file, bool collapse)
-        {
-            var anchroPoint = new List<string>();
-            MarkdownDocument document = new();
-            document.Parse(File.ReadAllText(file).Replace("\\|", "&#124;"));
-            //文章标题（首个标题的文本）
-            string title = null;
-            //文章内的目录
-            var sideNav = string.Empty;
-            //文章正文
-            var content = string.Empty;
-            bool startCollapse = false;
-
-            var lastHeaderLevel = 0;
-            foreach (var item in document.Blocks)
-            {
-                var anchroPointCount = 0;
-                var html = string.Empty;
-                var headerText = string.Empty;
-
-                if (item.Type == MarkdownBlockType.Header && (item as HeaderBlock).HeaderLevel <= 3)
-                {
-                    //将标题转为不加锚点的 HTML 以求 InnerText
-                    html = item.ToHtml(file, string.Empty);
-                    XmlDocument xml = new();
-                    xml.LoadXml(html);
-                    headerText = xml.InnerText;
-
-                    //区分同一篇文章中的重复锚点
-                    var itemId = headerText.ToId(0);
-                    anchroPointCount = anchroPoint.Count(p => p == itemId);
-                    anchroPoint.Add(itemId);
-
-                    var header = item as HeaderBlock;
-                    for (int i = 0; i < header.HeaderLevel - lastHeaderLevel; i++)
-                    {
-                        sideNav += "\r\n<nav class='nav nav-pills flex-column'>";
-                    }
-                    for (int i = 0; i < lastHeaderLevel - header.HeaderLevel; i++)
-                    {
-                        sideNav += "\r\n</nav>";
-                    }
-
-                    title ??= headerText;
-                    sideNav += $"\r\n<a class='ml-{(header.HeaderLevel - 2) * 2}{(header.HeaderLevel == 1 ? " d-none" : "")} my-1 nav-link with-space' href='{itemId.ToAnchorPoint(anchroPointCount)}'>{headerText}</a>";
-                    lastHeaderLevel = header.HeaderLevel;
-                }
-                html = item.ToHtml(file, headerText, collapse, anchroPointCount);
-
-                #region collapse
-                //如果 collapse 为 true，则将 h2 下面的所有内容用 <div></div> 包裹起来
-                if (collapse && (item as HeaderBlock)?.HeaderLevel == 2 && startCollapse)
-                {
-                    content += "</div>";
-                    startCollapse = false;
-                }
-                #endregion
-
-                content += html;
-
-                #region collapse
-                if (collapse && (item as HeaderBlock)?.HeaderLevel == 2 && !startCollapse)
-                {
-                    content += "\r\n<div class='div-collapse p-2 px-4'>";
-                    startCollapse = true;
-                }
-                #endregion
-            }
-            #region collapse
-            if (collapse && startCollapse)
-            {
-                content += "</div>";
-            }
-            #endregion
-
-            for (int i = 0; i < lastHeaderLevel - 0; i++)
-            {
-                sideNav += "\r\n</nav>";
-            }
-            return (title?.Trim(), content?.Trim(), sideNav?.Trim());
         }
 
         /// <summary>
