834d9d0dbeb40fc40d03905e501889404c975941
Alvaro Diaz <alvarodiazher@gmail.com>
2024-11-17 19:15:20 +0100

    Optimization

--- SearchEngine/RedirectHtmlMiddleware.cs
+++ SearchEngine/RedirectHtmlMiddleware.cs
@@ -16,7 +16,7 @@ namespace SearchEngine
         {
             var requestedPath = context.Request.Path.Value;
 
-             if (requestedPath.Contains("//") || requestedPath.StartsWith("//") || requestedPath.Contains("://"))
+             if (requestedPath.Contains("//"))
              {
                  context.Response.StatusCode = 400; 
                  return; // 终止请求处理
